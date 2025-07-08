e49ba42ce34fa9c3ee67c90680a52bf08ea14a7c
Coined617 <coined617@proton.me>
2025-07-08 15:28:09 -0400

    Update and rename anchorsmartcontractscaffold to frontend/src/App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Twizted Metal League</h1>
+      <button>Create Tournament</button>
+      <button>Join Tournament</button>
+      <button>Borrow/Lend (Fluid/Solend)</button>
+      <p>More features coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
